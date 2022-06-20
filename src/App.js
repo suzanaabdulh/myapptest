28129b02fee04ffd2d6b6260140d098a1db5147b
suzanaabdulh <73528279+suzanaabdulh@users.noreply.github.com>
2022-06-20 10:56:42 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CoolClick Website
         </a>
       </header>
     </div>
